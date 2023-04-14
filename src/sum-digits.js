4ed2c18af86bc0fc006fdbbe2ab632a3ff7decdc
Maksim Sinelnikau <maksim.sinelnikau@gmail.com>
2023-04-14 17:13:04 +0300

    feat: add sum-digits task

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+	if (n < 10 ) return n;
+	const str = new String(n);
+	const sum = str.split('').reduce((acc, el) => acc + +el, 0)
+	return getSumOfDigits(sum);
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -12,8 +12,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+	if (arguments.length === 0) return 'Unable to determine the time of year!';
+
+	const date = arguments[0];
+	// if (date.constructor !== 'Date') {
+	// 	throw new Error('Invalid date!');
+	// }
+
+	const month = date.getMonth();
+	if (month <= 1 || month >= 11) return 'winter';
+	if (month <= 4) return 'spring';
+	if (month <= 7) return 'summer';
+	if (month <= 10) return 'autumn';
 }
 
 module.exports = {
