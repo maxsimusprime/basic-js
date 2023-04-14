ebf0439ddb6de0348957b6b35f50abff60ce040b
Maksim Sinelnikau <maksim.sinelnikau@gmail.com>
2023-04-14 15:04:21 +0300

    feat: add carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,8 +18,17 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+	if (arguments.length === 0 || typeof arguments[0] !== 'string') return false;
+
+	const sampleActivity = Number(arguments[0]);
+	if (isNaN(sampleActivity)) return false;
+	if (sampleActivity > 15 || sampleActivity <= 0) return false;
+
+	const LN2 = 0.693;  // логарифм от 2 
+	const age = Math.log(sampleActivity / MODERN_ACTIVITY) / (LN2 / HALF_LIFE_PERIOD);
+
+	return Math.ceil(Math.abs(age));
+// https://www.youtube.com/watch?v=ZG4ul30mH5w&list=PLxGo9dxQkqWA-SD1lyrtltt8KxfLk9z4f&index=22
 }
 
 module.exports = {
