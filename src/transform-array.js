e16252b741dcb2cd850f80695e13523987f79876
Maksim Sinelnikau <maksim.sinelnikau@gmail.com>
2023-04-16 21:33:50 +0300

    feat: add decition for what-season task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+	if (!(arr instanceof Array)) throw new Error("'arr' parameter must be an instance of the Array!");
+	const transformed = [...arr];
+
+	const nav = {
+		'--discard-next': function(i, arr) { if (arr[i+1]) arr.splice(i+1, 1) },
+		'--discard-prev': function(i, arr) { arr[i-1] ? arr.splice(i-1, 2) : arr.splice(i, 1)},  // +
+		'--double-next': function(i, arr) { arr[i+1] ? arr.splice(i, 1, arr[i+1]) : arr.splice(i, 1) },  // +
+		'--double-prev': function(i, arr) { arr[i-1] ? arr.splice(i, 1, arr[i-1]) : arr.splice(i, 1) } // +
+	}
+
+
+	let i = 0;
+	for (let i=0; i < arr.length; i++) {
+		if ( arr[i] in nav) {
+			i;
+			nav[arr[i]](i, transformed)
+		}
+	}
+	return transformed;
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -13,17 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function getSeason(/* date */) {
 	if (arguments.length === 0) return 'Unable to determine the time of year!';
-
 	const date = arguments[0];
-	// if (date.constructor !== 'Date') {
-	// 	throw new Error('Invalid date!');
-	// }
 
-	const month = date.getMonth();
-	if (month <= 1 || month >= 11) return 'winter';
-	if (month <= 4) return 'spring';
-	if (month <= 7) return 'summer';
-	if (month <= 10) return 'autumn';
+	// typeof date !== 'Date' // Object
+	// date instanceof Date
+	// Object.prototype.toString.call(date) !== '[object Date]'
+	// date.toTimeString === undefined
+	// 'getUTCMinutes' in date.__proto__
+
+	// В оригинальном объекте нет свойства toString, они реализуются через __proto__  
+	if (date instanceof Date && date.toTimeString !== undefined && !date.hasOwnProperty('toString')) {
+		const month = date.getMonth();
+		if (month <= 1 || month >= 11) return 'winter';
+		if (month <= 4) return 'spring';
+		if (month <= 7) return 'summer';
+		if (month <= 10) return 'autumn';
+	}
+	throw new Error('Invalid date!');
 }
 
 module.exports = {
